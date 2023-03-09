2825e38a7969a0522447104a4f884da52027f32e
luigimqf <luigi.jackson@gmail.com>
2023-03-09 16:31:50 -0300

    🎨 api key to .env

--- src/liveblocks.config.ts
+++ src/liveblocks.config.ts
@@ -2,7 +2,7 @@ import { createClient } from "@liveblocks/client";
 import {createRoomContext} from "@liveblocks/react";
 
 const client = createClient({
-  publicApiKey: process.env.API_KEY as string,
+  publicApiKey: process.env.REACT_APP_API_KEY as string,
 });
 
 export const { RoomProvider, useRoom } = createRoomContext(client);
